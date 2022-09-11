2c309f2502cf2e6f9c5a6fb659fe0389dd88bcf4
martin-ngigi <martinwainaina001@gmail.com>
2022-09-10 21:03:39 -0700

    Dashboad UI of admin and student done

--- studenthome.php
+++ studenthome.php
@@ -16,13 +16,48 @@ session_start();
 
 ?>
 
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Student Dashboard</title>
+	<meta charset="utf-8">
+	<title>Admin Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="admin.css">
+
+		<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-	<h1>Student Home</h1>
-	<a href="logout.php"> Logout</a>
+	<header class="header">
+		<a href="">Student Dashboard</a>
+		<div class="logout">
+			<a href="logout.php" class="btn btn-primary">Logout</a>
+		</div>
+	</header>
+	<aside>
+		<ul>
+			<li>
+				<a href="">My Courses</a>
+			</li>
+			<li>
+				<a href="">My Results</a>
+			</li>
+		</ul>
+	</aside>
+	<div class="content">
+		<h1>Sidebar Accordin</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
 </body>
 </html>
